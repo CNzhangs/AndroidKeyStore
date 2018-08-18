c072f811f045310495fea01822422c810b44dddb
Administrator <455977767@qq.com>
2018-08-18 22:45:58 +0800

    添加接口实现

--- library/src/main/java/com/zhangs/library/IKeyStoreService.java
+++ library/src/main/java/com/zhangs/library/IKeyStoreService.java
@@ -11,17 +11,21 @@ import java.security.NoSuchProviderException;
 public interface IKeyStoreService {
     /**
      * 创建一个key
+     *
      * @param alias
      * @return false则创建失败.true成功或已存在.
      */
     boolean createKey(String alias) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException;
+
     /**
      * 加密数据
-     * @param key   数据存储时的Key值
-     * @param value 需要加密的数据
+     *
+     * @param key      数据存储时的Key值
+     * @param value    需要加密的数据
      * @param callback 加密回调
      * @return
      */
     void encrypt(String key, String value, EncryptCallback callback);
-    void    decrypt(String key, DecryptCallback callback);
+
+    void decrypt(String key, DecryptCallback callback);
 }
--- library/src/main/java/com/zhangs/library/KeyStoreHelper.java
+++ library/src/main/java/com/zhangs/library/KeyStoreHelper.java
@@ -1,20 +1,37 @@
 package com.zhangs.library;
 
+import android.os.Build;
+
 import com.zhangs.library.callback.DecryptCallback;
 import com.zhangs.library.callback.EncryptCallback;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 public class KeyStoreHelper implements IKeyStoreService{
     IKeyStoreService keyStoreService;
+    KeyStoreHelper(){
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            keyStoreService = new KeyStoreBelowApi23Compat();
+        }else {
+            keyStoreService = new KeyStoreAboveApi23Compat();
+        }
+    }
+
+    @Override
+    public boolean createKey(String alias) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyStoreException {
+        return keyStoreService.createKey(alias);
+    }
+
+    @Override
+    public void encrypt(String key, String value, EncryptCallback callback) {
+        keyStoreService.encrypt(key,value,callback);
+    }
 
     @Override
     public void decrypt(String key, DecryptCallback callback) {
-
+        keyStoreService.decrypt(key,callback);
     }
-
-    @Override
-    public boolean encrypt(String key, String value, EncryptCallback callback) {
-        return false;
-    }
-
-
 }
